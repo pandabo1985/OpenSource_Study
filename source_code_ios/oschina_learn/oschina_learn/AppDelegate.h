58e7106156e928fa3fc323902c35d9914a8d7f23
pandabo <pandabo1985@gmail.com>
2014-07-11 20:55:05 +0800

    ios  oscine learn

---
+++ source_code_ios/oschina_learn/oschina_learn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  oschina_learn
+//
+//  Created by panda on 14-7-10.
+//  Copyright (c) 2014年 com.afayear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ source_code_ios/oschina_learn/oschina_learn/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  oschina_learn
+//
+//  Created by panda on 14-7-10.
+//  Copyright (c) 2014年 com.afayear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
